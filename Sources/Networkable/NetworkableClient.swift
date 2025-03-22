12275ab244ab30036da71115c0989761af9c13c6
Mehmet Tarhan <hi@memtarhan.com>
2025-03-22 21:19:59 +0000

    public NetworkableClient protocol

--- Sources/Networkable/NetworkableClient.swift
+++ Sources/Networkable/NetworkableClient.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-protocol NetworkableClient {
+public protocol NetworkableClient {
     /// Handles HTTP call and retrieve a response for given url with no authorization
     /// - Parameter url: The url HTTP call should be pointed to
     /// - Returns: Returns a response of given type
